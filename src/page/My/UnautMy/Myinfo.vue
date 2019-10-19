15f51aa9d357b8779d5cd10665b3a7a614b557ec
hufeifan <3064428578@qq.com>
2019-10-19 19:06:41 +0800

    'xg'

--- src/page/My/UnautMy/Myinfo.vue
+++ src/page/My/UnautMy/Myinfo.vue
@@ -1,145 +1,182 @@
 <template>
-  <div class="UnautMy">
-    <div class="container">
-      <div class="header">
-        <span>未授权</span>
-      </div>
+    <div class="UnautMy">
+        <div class="container">
+            <div class="header">
+                <span>未授权</span>
+            </div>
 
-      <div class="userinfo">
-        <div class="userinfo-left">
-          <img src="../../../images/my/user_default.png" alt />
+            <div class="userinfo">
+                <div class="userinfo-left">
+                    <img :src="userInfo.avatar | display_avatar" class="logo" v-if="userInfo.avatar"/>
+                    <img src="../../../images/my/user_default.png" v-else/>
+                </div>
+                <div class="userinfo-centre">
+                    <p class="name">
+                        {{userInfo.userName}}
+                    </p>
+                    <span class="phone">{{userInfo.userTel}}</span>
+                </div>
+                <div class="userinfo-right">
+                    <router-link to="/invitation">
+                        <svg>
+                            <use xlink:href="#icon-QRCode"/>
+                        </svg>
+                        <span style="color: #fff">邀请</span>
+                    </router-link>
+                </div>
+            </div>
         </div>
-        <div class="userinfo-centre">
-          <p class="name">
-            张无忌
-            <small>(0168)</small>
-          </p>
-          <span class="phone">111111111111</span>
+        <clxsd-cell :title="'通道收益'" :to="'/channel-profit'" :value="userInfo.lianPiaoVaule"  is-link icon="my-message"/>
+        <clxsd-cell :title="'广告收益'" :to="'/develop'" :value="userInfo.lianPiaoVaule"  is-link icon="my-message" style="margin-bottom: .2rem"/>
+        <ul class="unautMy-userlist">
+            <div style="margin-bottom:.2rem">
+                <clxsd-cell v-if="userInfo.sub_type === 0" :title="'角色选择'" :to="'/customer-choose-role'" is-link icon="my-collection"/>
+            </div>
+        </ul>
+        <div style="margin-bottom:.2rem">
+            <clxsd-cell :title="'消息通知'" :to="'/develop'" is-link icon="my-message"/>
+            <clxsd-cell :title="'个人信息'" :to="'/business-setting'" is-link icon="my-employee"/>
         </div>
-        <div class="userinfo-right">
-          <svg>
-            <use xlink:href="#icon-QRCode" />
-          </svg>
-          <span>邀请</span>
-        </div>
-      </div>
+        <clxsd-cell :title="'设置'" :to="'/setting'" is-link icon="my-setting"/>
+        <clxsd-foot-guide :user-type="4"/>
     </div>
-
-    <ul class="unautMy-userlist">
-      
-      <div style="margin-bottom:.2rem">
-        <clxsd-cell :title="'角色选择'" :to="'/customer-choose-role'" is-link icon="my-collection" />
-      </div>
-      </ul>
-      <div style="margin-bottom:.2rem">
-        <clxsd-cell :title="'消息通知'" :to="'/develop'" is-link icon="my-message" />
-        <clxsd-cell :title="'个人信息'" :to="'/business-setting'" is-link icon="my-employee" />
-      </div>
-      <clxsd-cell :title="'设置'" :to="'/setting'" is-link icon="my-setting" />
-
-    <clxsd-foot-guide :user-type="4" />
-  </div>
 </template>
 
 <script>
-import ClxsdCell from "@/components/common/Cell";
-export default {
-  name: "Myinfo",
-  components: {
-    ClxsdCell
-  }
-};
+    import ClxsdCell from "@/components/common/Cell";
+    import {mapState} from "vuex";
+
+    export default {
+        name: "Myinfo",
+        components: {
+            ClxsdCell
+        },
+        computed: {
+            ...mapState({
+                userInfo: state => {
+                    const currentInfo = state.CURRENTUSER.data
+                    return {
+                        userName: currentInfo.display_name || currentInfo.real_name || currentInfo.phone || '丢失信息',
+                        userTel: currentInfo.phone || '丢失信息',
+                        //role: currentInfo
+                        user_type: currentInfo.user_type,
+                        sub_type: currentInfo.sub_type,
+                        state: currentInfo.status,
+                        avatar: currentInfo.avatar
+                    }
+
+                },
+            }),
+            canShou() {
+                const userInfo = this.userInfo
+                console.log(userInfo)
+                return (userInfo.state === 1) && (
+                    userInfo.sub_type === 1 ||
+                    userInfo.sub_type === 2 ||
+                    userInfo.sub_type === 3)
+            }
+        }
+
+    };
 </script>
 
 <style lang="scss" scoped>
-.UnautMy {
-  background: #f4f5f5;
-  font-size: 0.34rem;
-}
-.container {
-  background: rgba(45, 162, 255, 1);
-  .header {
-    height: 0.88rem;
-    line-height: 0.88rem;
-    text-align: center;
-
-    span {
-      width: 1.09rem;
-      height: 0.35rem;
-      font-size: 0.37rem;
-      color: rgba(255, 255, 255, 1);
-    }
-  }
-  .userinfo {
-    display: flex;
-    height: 1.7rem;
-
-    .userinfo-left {
-      margin-left: 0.36rem;
-      margin-right: 0.32rem;
-      width: 1.28rem;
-      height: 1.28rem;
-      background: rgba(255, 255, 255, 1);
-      padding: 0.04rem;
-      border-radius: 0.06rem;
-      opacity: 0.9;
-      img {
-        display: block;
-        width: 1.2rem;
-        height: 1.2rem;
-      }
-    }
-
-    .userinfo-centre {
-      flex-grow: 1;
-      .name {
-        height: 0.32rem;
-        line-height: 1;
-        margin-top: 0.24rem;
-        margin-bottom: 0.18rem;
+    .UnautMy {
+        background: #f4f5f5;
         font-size: 0.34rem;
-        font-weight: 500;
-        color: rgba(255, 255, 255, 1);
+    }
 
-        small {
-          color: #fff;
-          font-weight: 200;
+    .container {
+        background: rgba(45, 162, 255, 1);
+
+        .header {
+            height: 0.88rem;
+            line-height: 0.88rem;
+            text-align: center;
+
+            span {
+                width: 1.09rem;
+                height: 0.35rem;
+                font-size: 0.37rem;
+                color: rgba(255, 255, 255, 1);
+            }
         }
-      }
 
-      .phone {
-        width: 1.8rem;
-        height: 0.22rem;
-        font-size: 0.28rem;
-        font-weight: 500;
-        color: rgba(255, 255, 255, 1);
-      }
-    }
-    .userinfo-right {
-      margin-right: 0.44rem;
-      font-size: 0.22rem;
-      padding-top: 0.24rem;
-      text-align: center;
+        .userinfo {
+            display: flex;
+            height: 1.7rem;
 
-      svg {
-        width: 0.54rem;
-        height: 0.54rem;
-      }
-      img {
-        display: block;
-        width: 0.54rem;
-        height: 0.54rem;
-        border: 0.01rem solid #000;
-      }
-      span {
-        display: block;
-        margin-top: 0.02rem;
-        color: rgba(255, 255, 255, 1);
-        font-size: 0.22rem;
-        font-weight: 500;
-      }
+            .userinfo-left {
+                margin-left: 0.36rem;
+                margin-right: 0.32rem;
+                width: 1.28rem;
+                height: 1.28rem;
+                background: rgba(255, 255, 255, 1);
+                padding: 0.04rem;
+                border-radius: 0.06rem;
+                opacity: 0.9;
+
+                img {
+                    display: block;
+                    width: 1.2rem;
+                    height: 1.2rem;
+                }
+            }
+
+            .userinfo-centre {
+                flex-grow: 1;
+
+                .name {
+                    height: 0.32rem;
+                    line-height: 1;
+                    margin-top: 0.24rem;
+                    margin-bottom: 0.18rem;
+                    font-size: 0.34rem;
+                    font-weight: 500;
+                    color: rgba(255, 255, 255, 1);
+
+                    small {
+                        color: #fff;
+                        font-weight: 200;
+                    }
+                }
+
+                .phone {
+                    width: 1.8rem;
+                    height: 0.22rem;
+                    font-size: 0.28rem;
+                    font-weight: 500;
+                    color: rgba(255, 255, 255, 1);
+                }
+            }
+
+            .userinfo-right {
+                margin-right: 0.44rem;
+                font-size: 0.22rem;
+                padding-top: 0.24rem;
+                text-align: center;
+
+                svg {
+                    width: 0.54rem;
+                    height: 0.54rem;
+                }
+
+                img {
+                    display: block;
+                    width: 0.54rem;
+                    height: 0.54rem;
+                    border: 0.01rem solid #000;
+                }
+
+                span {
+                    display: block;
+                    margin-top: 0.02rem;
+                    color: rgba(255, 255, 255, 1);
+                    font-size: 0.22rem;
+                    font-weight: 500;
+                }
+            }
+        }
     }
-  }
-}
 
 </style>
