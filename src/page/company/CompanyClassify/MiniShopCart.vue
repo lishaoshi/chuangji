c74f169fc96384c6472aa5b8d7ddb5ee24ec25ec
lishaoshi <lishaoshi@clyyw.cn>
2019-11-13 11:31:06 +0830

    修改购物车总金额为小于等于0时不显示配送信息

--- src/page/company/CompanyClassify/MiniShopCart.vue
+++ src/page/company/CompanyClassify/MiniShopCart.vue
@@ -3,7 +3,7 @@
 		<!-- <div class="fade"></div> -->
 		<div class="mini-shop-cart">
 			<div class="total_price">
-				<p><i>￥</i><i id="totalPrice">{{totalPrice}}</i><i class="textContent" v-if="isHasDistribution&&totalPrice>=0">(含配送费:{{shipping_fee}})</i><i class="textContent" v-else-if="!isHasDistribution&&totalPrice>=0">(免配送费)</i></p>
+				<p><i>￥</i><i id="totalPrice">{{totalPrice}}</i><i class="textContent" v-if="isHasDistribution&&totalPrice>=0">(含配送费:{{shipping_fee}})</i><i class="textContent" v-else-if="!isHasDistribution&&totalPrice>0">(免配送费)</i></p>
 				<p>已选<i id="totalProductCount">{{count}}</i>个产品</p>
 			</div>
 
